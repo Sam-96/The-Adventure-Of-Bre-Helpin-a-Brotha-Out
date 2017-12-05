136dde966dd0f0e7100a7a58462d73aa503e199f
Samuel (Yomi) <yomi.akinmulero@gmail.com>
2017-12-04 20:29:57 -0800

    Initial commit. Bre can run in 4 directions.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private float vert;
+    private float jump;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vert = Input.GetAxis("Vertical");
+        anim.SetFloat("sprint", vert);
+        jump = Input.GetAxis("Jump");
+        anim.SetFloat("jump", jump);
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+    }
+
+}
