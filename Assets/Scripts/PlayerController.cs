9295a6d8d149cf99020a300869db2c80f7687537
cavertime06 <ray.caver@gmail.com>
2017-12-11 22:02:30 -0800

    Fully functional Bre

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,45 +5,103 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Animator anim;
-    private float vert;
-    private float jump;
+    public float speed = 0.5f;
+    public int forceConst = 5;
+    public float distToGround;
+    private bool canJump;
+    public bool IsGrounded;
+    private Rigidbody selfRigidbody;
 
     // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
+        selfRigidbody = GetComponent<Rigidbody>();
 
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        IsGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        IsGrounded = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+
+        //Character Walk
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Sprint", true);
+            anim.SetBool("Idle", false);
+        }
+
+        //Character Stop 
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Sprint", false);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Vector3.forward * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position -= Vector3.left * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if ((Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow)))
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Sprint", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                anim.SetBool("Kick", true);
+                anim.SetBool("Idle", false);
+            }
+            if (Input.GetKeyUp(KeyCode.K))
+            {
+                anim.SetBool("Kick", false);
+                anim.SetBool("Idle", true);
+            }
     }
 
     // Update is called once per frame
     void Update()
     {
-        vert = Input.GetAxis("Vertical");
-        anim.SetFloat("sprint", vert);
-        jump = Input.GetAxis("Jump");
-        anim.SetFloat("jump", jump);
-
-
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.Space) && (IsGrounded == true))
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            canJump = true;
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.rotation = Quaternion.Euler(0, 90, 0);
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.rotation = Quaternion.Euler(0, 270, 0);
-        }
-
+      
     }
 
 }
